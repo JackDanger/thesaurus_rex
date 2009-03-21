062b4342dff81af01f121b453fe756d365fc3545
Jack Danger Canty <gitcommit@6brand.com>
2009-03-21 10:08:52 -0700

    using shades of red

--- thesaurus_rex.js
+++ thesaurus_rex.js
@@ -24,7 +24,7 @@ ThesaurusRex = (function(){
   highlight = function(word, level){
     // assumes white background
     hex = (16 - ((level > 16) ? 16 : level)).toString(16);
-    return "<span style='background-color: #"+hex+hex+hex+"'; style='ThesaurusRexified'>"+word+"</span>"
+    return "<span style='background-color: #F"+hex+hex+"'; style='ThesaurusRexified'>"+word+"</span>"
   }
 
   growl = function(text, updateFunction){
